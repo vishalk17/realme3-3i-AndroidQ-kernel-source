4b6d4a7cd6ef4c61bb1eee931edeeb463daa1c46
vishalk17 <vishalkapadi95@gmail.com>
2020-07-30 03:36:41 +0000

    update build script

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 
 #git clone https://github.com/usmanmughalji/santoni-msm-4.9.git --single-branch --branch 4.9r1 kernel
 
-#git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-17.1 kernel/gcc-64 --depth=1
-#git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-17.1 kernel/gcc-32 --depth=1
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-17.1 gcc-64 --depth=1
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-17.1 gcc-32 --depth=1
 
 rm -rf out
+rm -rf build1.log
 
 mkdir -p out
 #make O=out clean && make O=out mrproper
@@ -14,6 +15,6 @@ make O=out ARCH=arm64 oppo6771_defconfig
 
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-                      CROSS_COMPILE="$(pwd)/aarch64-linux-android-4.9//bin/aarch64-linux-android-" \
-                      CROSS_COMPILE_ARM32="$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-" \
-                      2>&1 | tee build1.log
+                      CROSS_COMPILE="$(pwd)/gcc-64/bin/aarch64-linux-android-" \
+                      CROSS_COMPILE_ARM32="$(pwd)/gcc-32/bin/arm-linux-androideabi-" \
+                      | tee build1.log
